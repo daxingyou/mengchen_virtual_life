c6d6b7e5a811541d606229c50abffb01e71952b1
liudian <hardy.liu@outlook.com>
2018-04-18 11:49:19 +0800

    add: 添加publish/subscribe模型demo

---
+++ app/Console/Commands/AmqpDemo/TestAmqpPsConsumer.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Console\Commands\AmqpDemo;
+
+use App\Console\BaseCommand;
+use Illuminate\Console\Command;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+class TestAmqpPsConsumer extends BaseCommand
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test:amqp-ps-consumer';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'amqp publish subscribe test consumer';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $rabbitmqHost = env('RABBITMQ_HOST');
+        $rabbitmqPort = env('RABBITMQ_PORT');
+        $rabbitmqUser = 'admin';
+        $rabbitmqPass = 'password';
+        $exName = 'ex-demo-ps';
+        $connection = new AMQPStreamConnection($rabbitmqHost, $rabbitmqPort, $rabbitmqUser, $rabbitmqPass);
+        $channel = $connection->channel();
+        $channel->exchange_declare($exName, 'fanout', false, false, false);
+        list($queueName, ,) = $channel->queue_declare("", false, false, true, false);   //声明一个随机名字的自动销毁的队列
+        $channel->queue_bind($queueName, $exName);  //fanout交换机忽略route key
+        echo ' [*] Waiting for messages. To exit press CTRL+C', "\n";
+
+        //$channel->basic_qos(null, 1, null);     //告知broker上一个消息未确认之前，只push一个消息过来
+        $channel->basic_consume($queueName, '', false, false, false, false, [$this, 'msgCallback']);
+        //第四个参数，关闭no_ack，关闭自动确认
+
+        while (count($channel->callbacks)) {
+            $channel->wait();
+        }
+
+        $channel->close();
+        $connection->close();
+    }
+
+    public function msgCallback(AMQPMessage $msg)
+    {
+        echo " [x] Received ", $msg->body, "\n";
+        sleep(substr_count($msg->body, '.'));
+        echo " [x] Done", "\n";
+        $msg->delivery_info['channel']->basic_ack($msg->delivery_info['delivery_tag']);
+    }
+}
---
+++ app/Console/Commands/AmqpDemo/TestAmqpPsProducer.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Console\Commands\AmqpDemo;
+
+use App\Console\BaseCommand;
+use Illuminate\Console\Command;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+class TestAmqpPsProducer extends BaseCommand
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test:amqp-ps-producer {msg=hello...}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'amqp publish subscribe test producer';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $msgData = $this->argument('msg');
+
+        $rabbitmqHost = env('RABBITMQ_HOST');
+        $rabbitmqPort = env('RABBITMQ_PORT');
+        $rabbitmqUser = 'admin';
+        $rabbitmqPass = 'password';
+        $exName = 'ex-demo-ps';
+        $connection = new AMQPStreamConnection($rabbitmqHost, $rabbitmqPort, $rabbitmqUser, $rabbitmqPass);
+        $channel = $connection->channel();
+        $channel->exchange_declare($exName, 'fanout', false, false, false);
+
+        $msg = new AMQPMessage($msgData, [
+            //'delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT,   //消息持久化
+        ]);
+
+        $channel->basic_publish($msg, $exName);
+
+        $this->logInfo('Sent msg');
+        $channel->close();
+        $connection->close();
+    }
+}
